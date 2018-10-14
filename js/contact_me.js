5ef426365225e9c133eebf1f2b847402460bcdf3
Aaron Miller <millerac333@gmail.com>
2018-10-14 17:58:01 -0500

    first commit

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,5 +1,4 @@
 $(function() {
-
   $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
     preventSubmit: true,
     submitError: function($form, event, errors) {
@@ -10,12 +9,14 @@ $(function() {
       // get values from FORM
       var name = $("input#name").val();
       var email = $("input#email").val();
-      var phone = $("input#phone").val();
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
-      if (firstName.indexOf(' ') >= 0) {
-        firstName = name.split(' ').slice(0, -1).join(' ');
+      if (firstName.indexOf(" ") >= 0) {
+        firstName = name
+          .split(" ")
+          .slice(0, -1)
+          .join(" ");
       }
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
@@ -24,32 +25,43 @@ $(function() {
         type: "POST",
         data: {
           name: name,
-          phone: phone,
           email: email,
           message: message
         },
         cache: false,
         success: function() {
           // Success message
-          $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+          $("#success").html("<div class='alert alert-success'>");
+          $("#success > .alert-success")
+            .html(
+              "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+            )
             .append("</button>");
-          $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
-          $('#success > .alert-success')
-            .append('</div>');
+          $("#success > .alert-success").append(
+            "<strong>Your message has been sent. </strong>"
+          );
+          $("#success > .alert-success").append("</div>");
           //clear all fields
-          $('#contactForm').trigger("reset");
+          $("#contactForm").trigger("reset");
         },
         error: function() {
           // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+          $("#success").html("<div class='alert alert-danger'>");
+          $("#success > .alert-danger")
+            .html(
+              "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+            )
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-          $('#success > .alert-danger').append('</div>');
+          $("#success > .alert-danger").append(
+            $("<strong>").text(
+              "Sorry " +
+                firstName +
+                ", it seems that my mail server is not responding. Please try again later!"
+            )
+          );
+          $("#success > .alert-danger").append("</div>");
           //clear all fields
-          $('#contactForm').trigger("reset");
+          $("#contactForm").trigger("reset");
         },
         complete: function() {
           setTimeout(function() {
@@ -60,16 +72,16 @@ $(function() {
     },
     filter: function() {
       return $(this).is(":visible");
-    },
+    }
   });
 
-  $("a[data-toggle=\"tab\"]").click(function(e) {
+  $('a[data-toggle="tab"]').click(function(e) {
     e.preventDefault();
     $(this).tab("show");
   });
 });
 
 /*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-  $('#success').html('');
+$("#name").focus(function() {
+  $("#success").html("");
 });
